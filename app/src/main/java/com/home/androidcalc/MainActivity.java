955ef55a121b2c84c0d5c7141de9ce36eef48736
Evgenii <evgenii2451010@gmail.com>
2021-02-02 14:20:51 +0700

    Макет, первый набросок

--- app/src/main/java/com/home/androidcalc/MainActivity.java
+++ app/src/main/java/com/home/androidcalc/MainActivity.java
@@ -3,6 +3,9 @@ package com.home.androidcalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = findViewById(R.id.imageView);
+
+        Button buttonC = findViewById(R.id.ButtonC);
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
+
 }
