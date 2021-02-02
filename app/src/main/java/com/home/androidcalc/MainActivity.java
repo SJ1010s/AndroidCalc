8fe5bae585e1aeb91fb22a920bf6aec285a54f96
Evgenii <evgenii2451010@gmail.com>
2021-02-02 16:01:24 +0700

    Исправление проблемы с отображением фонового рисунка Отключение клавиатуры

--- app/src/main/java/com/home/androidcalc/MainActivity.java
+++ app/src/main/java/com/home/androidcalc/MainActivity.java
@@ -2,9 +2,12 @@ package com.home.androidcalc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,14 +17,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageBitmap(BitmapFactory.decodeResource(this.getResources(), R.drawable.android_calc_image));
 
         Button buttonC = findViewById(R.id.ButtonC);
         buttonC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
+
+        EditText editText = findViewById(R.id.textIn);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            editText.setShowSoftInputOnFocus(false);
+        } else {
+            editText.setTextIsSelectable(true);
+        }
     }
 
 }
